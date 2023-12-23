838e2881580227f40eb7ea15cdda259118b79051
Natsir <hentihu.natsir@gmail.com>
2023-12-23 21:15:06 +0700

    update cart

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import { IoMdCart } from "react-icons/io";
-import { IoFastFoodOutline } from "react-icons/io5";
+import { IoFastFoodOutline, IoTicket } from "react-icons/io5";
+import { useState } from "react";
 
 export default function App() {
+  const [showModal, setShowModal] = useState(false);
+
+  function toggleShowModal() {
+    setShowModal(!showModal);
+  }
+
   return (
-    <div className="px-8 pt-8 bg-slate-100 h-screen">
-      <Header />
+    <div className="px-8 pt-8 bg-slate-200 h-full relative">
+      <Header toggleShowModal={toggleShowModal} />
       <Card />
+      {showModal && (
+        <div className="fixed top-0 right-0 w-full h-full flex items-center justify-end z-50 bg-black bg-opacity-50">
+          <div className="relative bg-white h-full p-8 right-0">
+            <Carts toggleShowModal={toggleShowModal} />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
 
-function Header() {
+function Header({ toggleShowModal }) {
   return (
     <div className="header flex justify-between">
       <div className="left flex">
@@ -20,15 +34,17 @@ function Header() {
         </span>
       </div>
       <div className="right">
-        <button className="border-2 border-cyan-600 rounded w-36 h-8">
-          <IoMdCart className="inline-block mr-2" />
+        <button
+          className="border-2 border-cyan-600 hover:bg-cyan-600 text-cyan-600 hover:text-white rounded w-36 h-8 items-center"
+          onClick={() => toggleShowModal()}
+        >
+          <IoMdCart className="inline-block mr-2 " />
           Keranjang
         </button>
       </div>
     </div>
   );
 }
-
 function Card() {
   return (
     <div className="flex flex-wrap gap-4 mt-6 ">
@@ -61,6 +77,99 @@ function Cards({ children, imageUrl }) {
           + Tambahkan Ke Keranjang
         </button>
       </div>
+    </div>
+  );
+}
+
+function Carts({ toggleShowModal }) {
+  return (
+    <div className="flex flex-col justify-between h-full">
+      <div className="flex flex-col gap-2">
+        <div className="header flex flex-col gap-4 ">
+          <div>
+            <div className="flex flex-row justify-between">
+              <span className="flex items-center text-xl">
+                <IoMdCart className="mr-1 text-2xl text-cyan-600" />
+                Food Cart
+              </span>
+              <button onClick={() => toggleShowModal()}>X</button>
+            </div>
+          </div>
+          <hr></hr>
+          <div className="cart-list">
+            <CartItem />
+            <CartItem />
+          </div>
+        </div>
+        <hr></hr>
+        <div className="voucher flex flex-col gap-2 mt-2">
+          <span className="flex items-center text-sm">
+            <IoTicket className="mr-1 text-base text-cyan-600" />
+            Tambah Voucher
+          </span>
+          <input
+            className="border-2 rounded text-sm"
+            type="text"
+            id="fname"
+            name="fname"
+            placeholder="Masukkan vouchermu disini..."
+          />
+        </div>
+        <hr></hr>
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <div className="flex flex-row justify-between items-center h-10 px-3 bg-gray-200">
+          <span>Total</span>
+          <span>Rp 20.000</span>
+        </div>
+        <button className="w-full h-10 bg-cyan-600 rounded text-white">
+          Buat Pesanan
+        </button>
+      </div>
+    </div>
+  );
+}
+
+function CartItem() {
+  return (
+    <div className="flex flex-col mb-4">
+      <div className="cart-content flex mb-2">
+        <img
+          className="object-cover w-24 h-28 mr-2"
+          src="./images/mie-goreng.jpg"
+          alt="gambar makanan"
+        />
+        <div className="cart-detail flex flex-col justify-between w-80">
+          <div className="flex flex-col mb-2">
+            <span className="text-base">Mie Goreng</span>
+            <span className="text-sm">Rp 10.000</span>
+          </div>
+          <div className="cart-note-qty flex justify-between items-center text-ellipsis w-80 max-h-20 text-sm">
+            <div>
+              <p className="leading-none">
+                noteeeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeee
+              </p>
+            </div>
+            <div className="qty flex gap-4">
+              <button className="bg-cyan-600 text-white w-5 h-5 rounded">
+                -
+              </button>
+              <span>qty</span>
+              <button className="bg-cyan-600 text-white w-5 h-5 rounded">
+                +
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <input
+        className="border-2 rounded text-sm"
+        type="text"
+        id="fname"
+        name="fname"
+        placeholder={"Masukkan catatan disini..."}
+      />
     </div>
   );
 }
